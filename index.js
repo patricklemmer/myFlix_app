54a97e8fcd90842181073ba1a5b025dc12bb522d
meerkatstudio <patrick.lemmer@yahoo.com>
2022-05-11 21:52:28 +0200

    added request log and error middleware functions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //Requires "express" and "morgan"
-const express = require('express');
+const express = require('express'),
+  morgan = require('morgan');
 
 //Assigns express() to var "app"
 const app = express();
@@ -7,8 +8,10 @@ const app = express();
 //Assigns PORT # to var "PORT"
 const PORT = 8080;
 
-//"app.use" invokes Middleware functions
+//Invokes express.static to serve static files from folder "/public"
 app.use(express.static(__dirname + '/public'));
+//Invokes morgan to log URL requests to console
+app.use(morgan('common'));
 
 //Creates JSON object for endpoint "/movies"
 let topMovies = [{ title: 'Narcos', director: '' }, { title: 'Narcos Mexico', director: '' }, { title: 'Breaking Bad', director: '' }, { title: 'Die Hard', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }];
@@ -20,6 +23,12 @@ app.get('/movies', (req, res) => {
 
 app.get('/', (req, res) => {
   res.send('Netflix sucks');
+});
+
+//Error handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
 
 //Listens to server PORT and logs msg to console
