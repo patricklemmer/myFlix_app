5ca2484cfe0dcdb4819ad95d10b9bccd6c51d3d5
meerkatstudio <patrick.lemmer@yahoo.com>
2022-05-11 16:19:09 +0200

    changed name of documentation.html

--- index.js
+++ index.js
@@ -1,25 +1,64 @@
-//Requiring Express
-const express = require('express'),
-  path = require('path');
+// //Requiring Express
+// const express = require('express');
 
-//Setting Express to var "app"
+// //Setting Express to var "app"
+// const app = express();
+
+// let topMovies = [{ title: 'Narcos', director: '' }, { title: 'Narcos Mexico', director: '' }, { title: 'Breaking Bad', director: '' }, { title: 'Die Hard', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }];
+
+// //Serving static files from "public" folder using express "static" function
+// app.use(express.static('public'));
+
+// //GET requests
+// app.get('/movies', (req, res) => {
+//   res.json(topMovies);
+// });
+
+// app.get('/', (req, res) => {
+//   res.send('Netflix sucks')
+// });
+
+// //Server PORT
+// app.listen(8080, () => {
+//   console.log('Server running...');
+// });
+
+// const express = require('express');
+// const app = express();
+// const PORT = 8080;
+
+// app.use(express.static('public'));
+
+// let topMovies = [{ title: 'Narcos', director: '' }, { title: 'Narcos Mexico', director: '' }, { title: 'Breaking Bad', director: '' }, { title: 'Die Hard', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }];
+
+// app.get('/movies', (req, res) => {
+//   res.json(topMovies);
+// });
+
+// app.get('/', (req, res) => {
+//   res.send('Netflix sucks');
+// });
+
+// app.listen(PORT, () => console.log('Server is running....'));
+
+const express = require('express');
 const app = express();
+
+app.use(express.static('public'));
 
 let topMovies = [{ title: 'Narcos', director: '' }, { title: 'Narcos Mexico', director: '' }, { title: 'Breaking Bad', director: '' }, { title: 'Die Hard', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }];
 
-//GET requests
-app.get('/movies', (req, res) => {
+app.get('/api/movies', (req, res) => {
   res.json(topMovies);
 });
 
 app.get('/', (req, res) => {
-  res.send('Netflix sucks')
+  res.send('Some text');
 });
 
-//Serving static files from "public" folder using express "static" function
-app.use(express.static('/public'));
+app.use('/document', express.static(path.join(__dirname, 'document')));
+app.use('/', express.static(path.join(__dirname, 'public')));
 
-//Server PORT
 app.listen(8080, () => {
   console.log('Server running...');
 });
