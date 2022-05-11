44b171a800a5310867e028fb2a593877ee9cf345
meerkatstudio <patrick.lemmer@yahoo.com>
2022-05-11 19:19:58 +0200

    improved commentary in index.js

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
+//Requires "express" and "morgan"
 const express = require('express');
+
+//Assigns express() to var "app"
 const app = express();
+
+//Assigns PORT # to var "PORT"
 const PORT = 8080;
 
+//"app.use" invokes Middleware functions
 app.use(express.static(__dirname + '/public'));
 
+//Creates JSON object for endpoint "/movies"
 let topMovies = [{ title: 'Narcos', director: '' }, { title: 'Narcos Mexico', director: '' }, { title: 'Breaking Bad', director: '' }, { title: 'Die Hard', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }];
 
+//Get functions
 app.get('/movies', (req, res) => {
   res.json(topMovies);
 });
@@ -14,4 +22,5 @@ app.get('/', (req, res) => {
   res.send('Netflix sucks');
 });
 
+//Listens to server PORT and logs msg to console
 app.listen(PORT, () => console.log('Server is running....'));
