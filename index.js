2a0c29d0c371e81e465339b18c0f13926279b3d0
meerkatstudio <patrick.lemmer@yahoo.com>
2022-05-11 16:20:50 +0200

    updated index.js file

--- index.js
+++ index.js
@@ -23,42 +23,20 @@
 //   console.log('Server running...');
 // });
 
-// const express = require('express');
-// const app = express();
-// const PORT = 8080;
-
-// app.use(express.static('public'));
-
-// let topMovies = [{ title: 'Narcos', director: '' }, { title: 'Narcos Mexico', director: '' }, { title: 'Breaking Bad', director: '' }, { title: 'Die Hard', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }];
-
-// app.get('/movies', (req, res) => {
-//   res.json(topMovies);
-// });
-
-// app.get('/', (req, res) => {
-//   res.send('Netflix sucks');
-// });
-
-// app.listen(PORT, () => console.log('Server is running....'));
-
 const express = require('express');
 const app = express();
+const PORT = 8080;
 
 app.use(express.static('public'));
 
 let topMovies = [{ title: 'Narcos', director: '' }, { title: 'Narcos Mexico', director: '' }, { title: 'Breaking Bad', director: '' }, { title: 'Die Hard', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }, { title: '', director: '' }];
 
-app.get('/api/movies', (req, res) => {
+app.get('/movies', (req, res) => {
   res.json(topMovies);
 });
 
 app.get('/', (req, res) => {
-  res.send('Some text');
+  res.send('Netflix sucks');
 });
 
-app.use('/document', express.static(path.join(__dirname, 'document')));
-app.use('/', express.static(path.join(__dirname, 'public')));
-
-app.listen(8080, () => {
-  console.log('Server running...');
-});
+app.listen(PORT, () => console.log('Server is running....'));
