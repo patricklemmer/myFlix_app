1f956299138a61dd9af154b7e444e99602ca795b
meerkatstudio <patrick.lemmer@yahoo.com>
2022-05-20 12:53:36 +0200

    updated user schema

--- models.js
+++ models.js
@@ -21,7 +21,7 @@ let userSchema = mongoose.Schema({
   Username: { type: String, required: true },
   Password: { type: String, required: true },
   Email: { type: String, required: true },
-  Born: Date,
+  Birthday: Date,
   FavouriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }],
 });
 
