b1d9f3a48850ccd1efe060dd125ebd08b9b36280
meerkatstudio <patrick.lemmer@yahoo.com>
2022-05-20 12:54:54 +0200

    updated /users endpoint with Mongoose

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const express = require('express'),
   bodyParser = require('body-parser'),
   mongoose = require('mongoose'),
   Models = require('./models.js');
+const res = require('express/lib/response');
 
 //Assigns model names from model.js
 const Movies = Models.Movie;
@@ -300,18 +301,55 @@ let users = [
 //In order of CRUD
 
 //CREATE
-//Allow new users to register
-app.post('/users', (req, res) => {
-  const newUser = req.body;
 
-  if (newUser.name) {
-    newUser.id = uuid.v4();
-    users.push(newUser);
-    res.status(201).json(newUser);
-  } else {
-    res.status(400).send('Please add a name.');
-  }
+//Allow new users to register
+// JSON format is epxpected
+// {
+//   ID: Integer,
+//   Username: String,
+//   Password: String,
+//   Email: String,
+//   Birthday: Date
+//   }
+
+app.post('/users', (req, res) => {
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists.');
+      } else {
+        Users.create({
+          Username: req.body.Username,
+          Password: req.body.Password,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday,
+        })
+          .then((user) => {
+            res.status(201).json(user);
+          })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          });
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
 });
+
+// app.post('/users', (req, res) => {
+//   const newUser = req.body;
+
+//   if (newUser.name) {
+//     newUser.id = uuid.v4();
+//     users.push(newUser);
+//     res.status(201).json(newUser);
+//   } else {
+//     res.status(400).send('Please add a name.');
+//   }
+// });
 
 //CREATE
 //Allow users to add a movie to their list of favourites
