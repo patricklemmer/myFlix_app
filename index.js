fa90c60b71d7deff7cdb8e3870a3597299822b23
meerkatstudio <patrick.lemmer@yahoo.com>
2022-07-08 11:01:34 +0200

    updated index route response

--- index.js
+++ index.js
@@ -57,7 +57,10 @@ require('./passport');
 
 //Index route
 app.get('/', (req, res) => {
-  res.status(200).send('Welcome to myFlix');
+  res
+    .status(200)
+    .header('Access-Control-Allow-Origin', '*')
+    .send('Welcome to myFlix');
 });
 
 //CREATE
